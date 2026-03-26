253057f51b5e5ee0eef9068c2e7cecb5aa4154b1
Dumitru Gherman-Lad <thereallove@gmail.com>
2026-03-25 21:14:44 -0400

    feat: add migration for DownloadDirId column on HistoryCleanupItems

---
+++ backend/Database/Migrations/20260326011425_AddDownloadDirIdToHistoryCleanup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NzbWebDAV.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDownloadDirIdToHistoryCleanup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DownloadDirId",
+                table: "HistoryCleanupItems",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DownloadDirId",
+                table: "HistoryCleanupItems");
+        }
+    }
+}
